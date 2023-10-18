ad782be53cc67ee5837c42bdc39add8b880e635e
marcescala <marcescala@yahoo.com>
2023-10-17 20:22:51 -0500

    implementacion de sap marce

---
+++ src/firebase/inicio.js
@@ -0,0 +1,12 @@
+firebase.initializeApp({
+
+    apiKey: "AIzaSyBr5Z4iOCJqUFUVsIJYjm4-fS6ZYJTalls",
+    authDomain: "habitate-5e51d.firebaseapp.com",
+    projectId: "habitate-5e51d",
+    storageBucket: "habitate-5e51d.appspot.com",
+    messagingSenderId: "700997763612",
+    appId: "1:700997763612:web:2c069c2a7f4b49a316d5d9",
+    measurementId: "G-W5HYR984QH"
+    });
+    
+    const auth = firebase.auth();
