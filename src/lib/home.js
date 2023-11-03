f20889a03d8a959c4624d29bc6cb3006c9ffbd34
barbara-contreras <bcontreraslagos@gmail.com>
2023-11-03 17:03:02 -0300

    Cambios css - fun edi y signout

--- src/lib/home.js
+++ src/lib/home.js
@@ -1,9 +1,13 @@
+import { cerrarSesion } from './index.js';
+
 export const renderHome = (navigateTo) => {
   const section = document.createElement('section');
 
   const template = `
-    <button id="log-out" class="log-out"> Cerrar Sesión </button>
-    <img class="logo" src="Images/logo_habitate_largo.png">
+    <img class="logo-home" src="Images/logo_habitate_largo.png">
+    <div class="container-buttonout"> 
+        <button id="log-out" class="log-out"> Cerrar Sesión </button>
+    </div>    
         <h2 class="title-home"> ¿Qué quieres ver?</h2>
         <div class="buttons">
            <div class="up">
@@ -21,10 +25,6 @@ export const renderHome = (navigateTo) => {
                 <img src="Images/habitos_habitate.png" class="home-images">
                 <span> Hábitos Saludables </span>
             </button>
-            <button id="button-curiosities" class="button-curiosities" name="datos-curiosos" > 
-                <img src="Images/curiosos_habitate.png" class="home-images">
-                <span> Datos Curiosos </span>
-            </button> 
            </div>
         </div>
         <div class="footer">
@@ -36,6 +36,7 @@ export const renderHome = (navigateTo) => {
 
   const buttonLogOut = section.querySelector('#log-out');
   buttonLogOut.addEventListener('click', () => {
+    cerrarSesion();
     navigateTo('/');
   });
   const buttonGoWallRecipes = section.querySelector('#button-recipes');
@@ -48,10 +49,6 @@ export const renderHome = (navigateTo) => {
   });
   const buttonGoWallHabits = section.querySelector('#button-habits');
   buttonGoWallHabits.addEventListener('click', () => {
-    navigateTo('/wall');
-  });
-  const buttonGoWallCuriosities = section.querySelector('#button-curiosities');
-  buttonGoWallCuriosities.addEventListener('click', () => {
     navigateTo('/wall');
   });
   return section;
--- test/index.spec.js
+++ test/index.spec.js
@@ -57,17 +57,3 @@ describe('button Go', (done) => {
     });
   });
 });
-test('have a button', () => {
-  const DOM = document.createElement('section');
-  DOM.append(renderSignup());
-  const haveAButton = DOM.querySelector('#go-login');
-  expect(haveAButton).not.toBe(undefined);
-});
-test('después de dar click botón Go login llamar función NavigateTo', () => {
-  const DOM = document.createElement('section');
-  const navigateTo = jest.fn();
-  DOM.append(renderSignup(navigateTo));
-  const buttonGo = DOM.querySelector('#go-login');
-  buttonGo.click();
-  expect(navigateTo).toHaveBeenCalledTimes(1);
-});
